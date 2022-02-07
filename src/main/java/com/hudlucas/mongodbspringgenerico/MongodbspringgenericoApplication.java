26c6f18d8c7bcdddeaf6048a461c9e775a2fc14c
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-02-07 13:30:16 -0300

    Created

---
+++ src/main/java/com/hudlucas/mongodbspringgenerico/MongodbspringgenericoApplication.java
@@ -0,0 +1,13 @@
+package com.hudlucas.mongodbspringgenerico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbspringgenericoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbspringgenericoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hudlucas/mongodbspringgenerico/MongodbspringgenericoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudlucas.mongodbspringgenerico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbspringgenericoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
