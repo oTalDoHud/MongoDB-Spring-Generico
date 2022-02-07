de33b9014d8f574094cfe82ce93031104a345b58
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-02-07 18:07:31 -0300

    instancieted database

---
+++ src/main/java/com/hudlucas/mongodbspringgenerico/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.hudlucas.mongodbspringgenerico.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.hudlucas.mongodbspringgenerico.model.entities.User;
+import com.hudlucas.mongodbspringgenerico.repositories.UserRepositoy;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepositoy userRepo;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepo.deleteAll();
+		
+		User hud = new User("Hudson Lucas", "Hudson@gmail.com");
+		User pp = new User("Peterson Santos", "Peterson@gmail.com");
+		User ka = new User("Katharine Araujo", "Katharine@gmail.com");
+		User luis = new User("Luis Antonio", "Luis@gmail.com");
+		
+		userRepo.saveAll(Arrays.asList(hud, pp, ka, luis));
+		
+	}
+
+}
