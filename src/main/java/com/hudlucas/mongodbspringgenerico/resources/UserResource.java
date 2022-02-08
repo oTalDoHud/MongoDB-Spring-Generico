f29ced5757fca9379bcf08ce777c5ef7b0a67dae
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-02-08 12:18:20 -0300

    User by id with exception handler

--- src/main/java/com/hudlucas/mongodbspringgenerico/resources/UserResource.java
+++ src/main/java/com/hudlucas/mongodbspringgenerico/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +22,18 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> listUser = service.findAll();
 		List<UserDTO> listDto = listUser.stream().map(x -> new UserDTO(x))
 				.collect(Collectors.toList());
 		
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
 		
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/com/hudlucas/mongodbspringgenerico/services/UserService.java
+++ src/main/java/com/hudlucas/mongodbspringgenerico/services/UserService.java
@@ -1,20 +1,29 @@
 package com.hudlucas.mongodbspringgenerico.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hudlucas.mongodbspringgenerico.model.entities.User;
 import com.hudlucas.mongodbspringgenerico.repositories.UserRepositoy;
+import com.hudlucas.mongodbspringgenerico.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepositoy userRepo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = userRepo.findById(id);
+
+		return user.orElseThrow(()-> new ObjectNotFoundException("Não foi possível encontar o objeto com o ID: " + id));
 	}
 }
